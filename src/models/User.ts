1524032d971a3dd6fe9d6a862a61e78febb1d113
Gojo Sataro <sunnyshinde2601@gmail.com>
2025-08-06 18:58:15 +0000

    failed to fetch ?? why this is hapenning what is causing this error tu i

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,7 +7,6 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, 'Please provide an email.'],
-    unique: true,
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       'Please add a valid email.',
